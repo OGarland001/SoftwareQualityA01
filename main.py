074fc327de826e35bfffe10a0bc48d9139f59808
Owen Garland <Ogarland9238@conestogac.on.ca>
2024-01-25 09:18:44 -0500

    added input data and comments

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #OwenG Assignment.
 
+# Read the excel sheet and print the content
+# create a loop that does some basic calculation for a million times
+# conver the above loop to be done by 8 mutliprocessing jobs. muti threaded
+# print out CPU temperature before and after both runs.
+# print the time takem by both execution(for loop  & parallelized run)
+# print the system properties of your PC
+# platform
+# CPU specifications
+# Number of CPUs
+# requirements.txt to hold all the package versions needed to run the script.
