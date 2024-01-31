c0660dd927c26a4ab9d4e2b9c9f2f2165fe021a9
Owen Garland <Ogarland9238@conestogac.on.ca>
2024-01-30 21:41:27 -0500

    final commit

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 #OwenG Assignment.
 
-# Read the excel sheet and print the content
-# create a loop that does some basic calculation for a million times
-# conver the above loop to be done by 8 mutliprocessing jobs. muti threaded
-# print out CPU temperature before and after both runs.
-# print the time takem by both execution(for loop  & parallelized run)
-# print the system properties of your PC
-# platform
-# CPU specifications
-# Number of CPUs
-# requirements.txt to hold all the package versions needed to run the script.
-
 import os, platform, psutil, xlrd, pandas as pd, time
 import concurrent.futures
 def display_system_properties():
